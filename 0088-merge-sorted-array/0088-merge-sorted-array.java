a3f85b72fba3dc98edeb5ff2ea3441393d3483a0
Chandra Prakash Yadav <chandraprakashy513@gmail.com>
2026-08-06 00:39:44 +0530

    Time: 0 ms (100%), Space: 43.8 MB (63.78%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void merge(int[] a, int m, int[] b, int n) {
+        int i=0;
+        int j=0;
+        int k=0;
+        int[] c = new int[m + n]; 
+        while(i<m && j<n)
+        {
+            if(a[i] < b[j])
+            {
+                c[k] = a[i];
+                i++;
+                k++;
+            }
+            else{
+                c[k] = b[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<m)
+        {
+            c[k] = a[i];
+            i++;
+            k++;
+        }
+        while(j<n)
+        {
+            c[k] = b[j];
+            j++;
+            k++;
+        }
+        for (int x = 0; x < m + n; x++) {
+            a[x] = c[x];
+        }
+    }
+}
