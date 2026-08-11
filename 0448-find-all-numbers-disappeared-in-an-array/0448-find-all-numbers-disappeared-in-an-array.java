d9804a55ec6ea1bf06c18ebcb0e19fd53843e497
Chandra Prakash Yadav <chandraprakashy513@gmail.com>
2026-08-11 23:19:34 +0530

    Time: 6 ms (83.16%), Space: 67.2 MB (64.34%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,37 +1,27 @@
 class Solution {
-
-    public static void swap(int[] arr, int i, int idx) {
+    public static void swap(int arr[] , int i,int idx)
+    {
         int temp = arr[i];
         arr[i] = arr[idx];
         arr[idx] = temp;
     }
-
     public List<Integer> findDisappearedNumbers(int[] arr) {
-
-        List<Integer> ans = new ArrayList<>();
-
-        int n = arr.length;
-        int i = 0;
-
-        while (i < n) {
-
-            int idx = arr[i] - 1;
-
-            if (arr[i] == arr[idx]) {
-                i++;
-            } 
-            else {
-                swap(arr, i, idx);
+        List<Integer> ans= new ArrayList<>();
+        int n=arr.length;
+        int i=0;
+        while(i<n)
+        {
+            int idx =arr[i]-1;
+            if(arr[i] == arr[idx]) i++;
+            else
+            {
+                swap(arr,idx,i);
             }
         }
-
-        for (int j = 0; j < n; j++) {
-
-            if (arr[j] != j + 1) {
-                ans.add(j + 1);
-            }
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j] !=j+1) ans.add(j+1);
         }
-
         return ans;
     }
 }
